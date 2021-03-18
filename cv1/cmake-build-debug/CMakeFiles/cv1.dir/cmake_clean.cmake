b2556b2474c7ca7a694e072dabd1dcd0dcfa5ac5
huangyekun <huangyekun@corp.netease.com>
2021-03-18 19:08:42 +0800

    1

---
+++ cv1/cmake-build-debug/CMakeFiles/cv1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv1.dir/main.cpp.obj"
+  "cv1.exe"
+  "cv1.exe.manifest"
+  "cv1.pdb"
+  "libcv1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cv1/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#inclue
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
