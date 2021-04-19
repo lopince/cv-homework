775e67dc81bbe5168127cdae7457041b8f3adcfd
huangyekun <huangyekun@corp.netease.com>
2021-04-19 16:15:11 +0800

    push

--- ex5/ex.py
+++ ex5/ex.py
@@ -22,14 +22,17 @@ def add_noise(img, snr=0.9):
     return np.asarray(temp_arr)
 
 
+# 读取原图，并依次转化为灰度图，二值图
 img = imgs.read('test.jpg')
 grey_img = imgs.to_grey_img(img)
 binary_img = binarizes.binary(grey_img)[1]
 imgs.write('binary.jpg', binary_img)
 
+# 对二值图添加噪声
 noised_img = add_noise(binary_img)
 print(type(noised_img))
 imgs.write('noised.jpg', noised_img)
 
+# 使用中值滤波方法进行降噪
 median_filtered_img = filters.median(noised_img)
 imgs.write('median_filtered.jpg', median_filtered_img)
